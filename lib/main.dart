67e1e9701b151fa2a8f213607d353ead7432bb75
e.abaev <e.abaev@gt-sot.ru>
2025-01-29 15:18:41 +0300

    реализована нормальная проверка авторизации

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'home_screen.dart';
+import 'login_screen.dart';
 import 'theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(
@@ -20,8 +22,32 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           title: 'Грузоперевозки',
           theme: themeProvider.currentTheme,
-          home: HomeScreen(),
+          home: CheckAuthScreen(),
         );
+      },
+    );
+  }
+}
+
+class CheckAuthScreen extends StatelessWidget {
+  Future<bool> _checkIfLoggedIn() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    return token != null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _checkIfLoggedIn(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.data == true) {
+          return HomeScreen();
+        } else {
+          return LoginScreen();
+        }
       },
     );
   }
