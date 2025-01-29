4a65910e9851a13d497cc8eae148b7933d43fbc8
e.abaev <e.abaev@gt-sot.ru>
2025-01-29 19:35:33 +0300

    готовая реализаия получения списка полетов в блоке "Популярные маршруты"

--- lib/search_screen.dart
+++ lib/search_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // Импортируем для работы с JSON
 import 'package:provider/provider.dart';
 import 'theme_provider.dart';
 
@@ -13,23 +15,14 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController _fromController = TextEditingController();
   TextEditingController _toController = TextEditingController();
-  List<String> popularDestinations = [
-    "Нью-Йорк",
-    "Лондон",
-    "Париж",
-    "Токио",
-    "Сидней",
-    "Берлин",
-    "Рим",
-    "Мадрид",
-    "Дубай",
-    "Торонто"
-  ];
+  List<dynamic> deliveryOrders = []; // Список для хранения данных о доставках
+  bool isLoading = true; // Состояние загрузки
 
   @override
   void initState() {
     super.initState();
     _loadCachedLocation();
+    _fetchDeliveryOrders(); // Получаем данные о доставках при инициализации
   }
 
   Future<void> _loadCachedLocation() async {
@@ -68,13 +61,60 @@ class _SearchScreenState extends State<SearchScreen> {
     });
   }
 
+  Future<void> _fetchDeliveryOrders() async {
+    final url = Uri.parse('http://212.109.196.24:5045/api/DeliveryOrders/GetAllDeliveryOrders');
+
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('token'); // Получаем токен из SharedPreferences
+
+      setState(() {
+        isLoading = true; // Устанавливаем состояние загрузки
+      });
+
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token', // Добавляем токен в заголовок
+        },
+      );
+
+      if (response.statusCode == 200) {
+        // Если запрос успешен, парсим ответ
+        final List<dynamic> jsonResponse = jsonDecode(response.body);
+
+        setState(() {
+          deliveryOrders = jsonResponse; // Сохраняем данные о доставках в список
+          isLoading = false; // Сбрасываем состояние загрузки
+        });
+      } else {
+        // Обработка ошибки при запросе
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Ошибка при получении данных: ${response.reasonPhrase}')),
+        );
+        setState(() {
+          isLoading = false; // Сбрасываем состояние загрузки даже в случае ошибки
+        });
+      }
+    } catch (e) {
+      // Обработка исключений
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Произошла ошибка: $e')),
+      );
+      setState(() {
+        isLoading = false; // Сбрасываем состояние загрузки даже в случае ошибки
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
     return Scaffold(
       body: Container(
-        color: themeProvider.isDarkMode ? Colors.black : Colors.white, // Фон в зависимости от темы
+        color: themeProvider.isDarkMode ? Colors.black : Colors.white,
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
@@ -88,7 +128,7 @@ class _SearchScreenState extends State<SearchScreen> {
             SizedBox(height: 20),
             Container(
               decoration: BoxDecoration(
-                color: themeProvider.isDarkMode ? Colors.grey[800] : Colors.blue, // Цвет фона блока ввода
+                color: themeProvider.isDarkMode ? Colors.grey[800] : Colors.blue,
                 borderRadius: BorderRadius.circular(8.0),
               ),
               padding: const EdgeInsets.all(16.0),
@@ -100,9 +140,9 @@ class _SearchScreenState extends State<SearchScreen> {
                       hintText: 'Откуда',
                       border: OutlineInputBorder(),
                       filled: true,
-                      fillColor: themeProvider.isDarkMode ? Colors.grey[700] : Colors.white, // Фон для текстового поля
+                      fillColor: themeProvider.isDarkMode ? Colors.grey[700] : Colors.white,
                     ),
-                    style: TextStyle(color: themeProvider.isDarkMode ? Colors.white : Colors.black), // Цвет текста
+                    style: TextStyle(color: themeProvider.isDarkMode ? Colors.white : Colors.black),
                   ),
                   SizedBox(height: 20),
                   TextField(
@@ -111,9 +151,9 @@ class _SearchScreenState extends State<SearchScreen> {
                       hintText: 'Куда',
                       border: OutlineInputBorder(),
                       filled: true,
-                      fillColor: themeProvider.isDarkMode ? Colors.grey[700] : Colors.white, // Фон для текстового поля
+                      fillColor: themeProvider.isDarkMode ? Colors.grey[700] : Colors.white,
                     ),
-                    style: TextStyle(color: themeProvider.isDarkMode ? Colors.white : Colors.black), // Цвет текста
+                    style: TextStyle(color: themeProvider.isDarkMode ? Colors.white : Colors.black),
                   ),
                   SizedBox(height: 20),
                   ElevatedButton(
@@ -129,58 +169,96 @@ class _SearchScreenState extends State<SearchScreen> {
             SizedBox(height: 20),
             Container(
               decoration: BoxDecoration(
-                color: themeProvider.isDarkMode ? Colors.grey[800] : Colors.blue, // Цвет фона блока популярных маршрутов
+                color: themeProvider.isDarkMode ? Colors.grey[800] : Colors.blue,
                 borderRadius: BorderRadius.circular(8.0),
               ),
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+              child:
+              Column(crossAxisAlignment:
+              CrossAxisAlignment.start,
+                children:[
                   Text(
                     'Популярные маршруты',
                     style:
-                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: themeProvider.isDarkMode ? Colors.white : Colors.black), // Цвет заголовка
+                    TextStyle(fontSize:
+                    18, fontWeight:
+                    FontWeight.bold, color:
+                    themeProvider.isDarkMode ? Colors.white : Colors.black),
                   ),
-                  SizedBox(height: 10),
-                  Container(
-                    height: 120, // Высота контейнера для скроллируемых объектов
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: popularDestinations.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 100, // Ширина каждого квадрата
-                          margin:
-                          EdgeInsets.symmetric(horizontal: 8.0),
-                          decoration:
-                          BoxDecoration(color:
-                          themeProvider.isDarkMode ? Colors.grey[800] : Colors.blue, // иконки
-                              borderRadius:
-                              BorderRadius.circular(8.0)),
-                          child:
-                          Column(mainAxisAlignment:
-                          MainAxisAlignment.center,
-                              children:[
-                                Expanded(child:
-                                ClipRRect(borderRadius:
-                                BorderRadius.circular(8.0),
-                                    child:
-                                    Image.network('https://yt3.ggpht.com/ytc/AKedOLS7Nzy4zKW-CoHoGHk3y1_lKxJwGAJ-tu_Wpf357Q=s900-c-k-c0x00ffffff-no-rj', fit:
-                                    BoxFit.cover))),
-                                SizedBox(height:
-                                5),
-                                Text(popularDestinations[index],
-                                    style:
-                                    TextStyle(color:
-                                    Colors.white)), // Цвет текста города под квадратом
-                              ]),
-                        );
-                      },
+                  SizedBox(height:
+                  10),
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 500), // Длительность анимации
+                    color:
+                    isLoading ? (themeProvider.isDarkMode ? Colors.grey[700] : Colors.grey[300]) : (themeProvider.isDarkMode ? Colors.grey[800] : Colors.blue), // Цвет фона в зависимости от состояния загрузки
+                    height:
+                    isLoading ? 120 : null, // Высота контейнера для скроллируемых объектов
+                    child:
+                    isLoading
+                        ? Center(child:
+                    CircularProgressIndicator()) // Индикатор загрузки во время запроса
+                        : Container( // Используем Container для ограничения высоты списка
+                      height: 120, // Ограничиваем высоту списка
+                      child:
+                      ListView.builder(
+                        scrollDirection:
+                        Axis.horizontal, // Устанавливаем горизонтальную прокрутку
+                        itemCount:
+                        deliveryOrders.length,
+                        itemBuilder:
+                            (context, index) {
+                          final order =
+                          deliveryOrders[index];
+                          final avatarBase64 =
+                          order['avatar']; // Получаем аватар в base64
+
+                          // Декодируем base64 строку в изображение
+                          ImageProvider avatarImage =
+                          avatarBase64.isNotEmpty ?
+                          MemoryImage(base64Decode(avatarBase64)) :
+                          AssetImage('assets/default_avatar.png'); // Путь к изображению по умолчанию
+
+                          return Container(
+                            width:
+                            100, // Ширина каждого элемента
+                            margin:
+                            EdgeInsets.symmetric(horizontal:
+                            8.0),
+                            decoration:
+                            BoxDecoration(color:
+                            themeProvider.isDarkMode ? Colors.grey[800] : Colors.blue,
+                                borderRadius:
+                                BorderRadius.circular(8.0)),
+                            child:
+                            Column(mainAxisAlignment:
+                            MainAxisAlignment.center,
+                                children:[
+                                  Expanded(child:
+                                  ClipRRect(borderRadius:
+                                  BorderRadius.circular(8.0),
+                                      child:
+                                      Image(image:
+                                      avatarImage, fit:
+                                      BoxFit.cover))),
+                                  SizedBox(height:
+                                  5),
+                                  Text('${order['from']} - ${order['to']}',
+                                      style:
+                                      TextStyle(color:
+                                      Colors.white)), // Цвет текста маршрута
+                                  Text(order['name'],
+                                      style:
+                                      TextStyle(color:
+                                      Colors.red)), // Цвет текста имени
+                                ]),
+                          );
+                        },
+                      ),
                     ),
-                  ),
+                  )
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),
